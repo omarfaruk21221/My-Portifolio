d5d50c303dfdd63be5a130285ba55d9f5acca6e3
omarfarukcodes69 <omarfaruk.codes69@gmail.com>
2025-12-06 03:55:40 +0600

    fix error

--- src/app/bannar/page.jsx
+++ src/app/bannar/page.jsx
@@ -49,6 +49,7 @@ export default function BannarPage() {
                         <a
                             href="https://drive.google.com/file/d/1-QcwwTf5xkK8m_smUfzJC0d_bD8af5l-/view?usp=sharing"
                             target="_blank"
+                            rel="noopener noreferrer"
                             className="flex items-center justify-center px-8 py-4 bg-gradient-to-r from-primary to-secondary text-white font-bold rounded-full shadow-lg hover:shadow-primary/40 hover:-translate-y-1 transition-all duration-300 transform"
                         >
                             <FaDownload className="mr-2 text-lg" />
